b06bc65a3854aa3b4357bb72b22aec1ada7495ef
Marcos Vinícyus Ramos Vieira <marcos.ramos15@outlook.com>
2024-03-29 17:00:29 -0300

    finalizei o ex44 e fiz o ex45

--- pacote-download/CursoemVideo/ex0044_gerenciador_pagamentos.py
+++ pacote-download/CursoemVideo/ex0044_gerenciador_pagamentos.py
@@ -1,3 +1,25 @@
 print('{:=^40}'.format('LOJAS MRV'))
 preço = float(input('Preço das compras: R$'))
-print()
+print('''FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opçao = int(input('Qual é a opção? '))
+if opçao == 1:
+    total = preço - (preço * 10 / 100)
+elif opçao == 2:
+    total = preço - (preço * 5 / 100)
+elif opçao == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${:.2f} SEM JUROS'.format(parcela))
+elif opçao == 4:
+    total = preço + (preço * 20 / 100)
+    totalparc = int(input('Quantas parcelas? '))
+    valorfinal = total / totalparc
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(totalparc, valorfinal))
+else:
+    total = preço
+    print('\033[31mOpção de pagamento INVÁLIDA. Tente novamente!\033[m')
+print("Sua compra de R${:.2f} vai custar R${:.2f}".format(preço, total))
